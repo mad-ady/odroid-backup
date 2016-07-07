a097f2c44ab44e9e9584b1a4d9a91d5e77888fc9
Adrian Popa <adrian.popa.gh@gmail.com>
2016-07-07 12:44:31 +0300

    Reworked dependencies, getRemovableDisks - in progress

--- odroid-backup.pl
+++ odroid-backup.pl
@@ -3,13 +3,54 @@ use strict;
 use warnings;
 
 my $dialog;
-my $sfdisk;
-my $fsarchiver;
+my %bin;
+
+my %dependencies = (
+'sfdisk' => 'sudo apt-get install sfdisk',
+'fsarchiver' => 'sudo apt-get install fsarchiver',
+'udevadm' => 'sudo apt-get install udev',
+);
 
 checkDependencies();
 checkUser();
 
-$dialog->msgbox(title => "Odroid Backup", text => "Passed prerequisites test");
+my $selection = $dialog->radiolist(title => "Odroid Backup", text => "Please select if you want to perform a backup or a restore:", 
+                    list => [   'backup', [ 'Backup partitions', 1],
+                                'restore', [ 'Restore partitions', 0] ]);
+                                
+print "$selection\n";
+
+if($selection eq 'backup'){
+    #get a list of removable drives and their partitions
+    getRemovableDisks();
+}
+if($selection eq 'restore'){
+    
+}
+
+sub getRemovableDisks{
+    opendir(my $dh, "/sys/block/") || die "Can't opendir /sys/block: $!";
+    while (readdir $dh) {
+        my $block = $_;
+        next if ($block eq '.' || $block eq '..');
+        print "/sys/block/$block\n";
+        my @info = `$bin{udevadm} info -a --path=/sys/block/$block`;
+        my $removable = 0;
+        my $model = "";
+        foreach my $line (@info){
+            if($line=~/ATTRS\{model\}==\"(.*)\"/){
+                $model = $1;
+            }
+            if($line=~/ATTRS\{removable\}==\"(.*)\"/){
+                $removable = $1;
+            }
+        }
+        
+        print "$block\t$model\t$removable\n";
+        
+    }
+    
+}
 
 sub checkUser{
     if($< != 0){
@@ -39,18 +80,12 @@ sub checkDependencies{
         $message .= "UI::Dialog missing - You can install it with sudo apt-get install libui-dialog-perl zenity dialog\n";
     }
     
-    $sfdisk = `which sfdisk`;
-    $sfdisk=~s/\s+|\r|\n//g;
-    
-    if($sfdisk eq ''){
-        $message .= "sfdisk missing - You can install it with sudo apt-get install sfdisk\n";
-    }
-    
-    $fsarchiver = `which fsarchiver`;
-    $fsarchiver=~s/\s+|\r|\n//g;
-    
-    if($fsarchiver eq ''){
-        $message .= "fsarchiver missing - You can install it with sudo apt-get install fsarchiver\n";
+    foreach my $program (sort keys %dependencies){
+        $bin{$program} = `which $program`;
+        $bin{$program}=~s/\s+|\r|\n//g;
+        if($bin{$program} eq ''){
+            $message .= "$program missing - You can install it with $dependencies{$program}\n";
+        }
     }
     
     if($message ne ''){

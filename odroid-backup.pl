485e609bfe4bf583eb0616956d2bed7581113b27
immanuelfodor <immanuelfactor+github@gmail.com>
2018-06-07 11:00:54 +0200

    adding missing dependency and log info, also fixes a quote error

--- odroid-backup.pl
+++ odroid-backup.pl
@@ -18,6 +18,7 @@ my %dependencies = (
 'blkid' => 'util-linux',
 'dd' => 'coreutils',
 'partclone.vfat' => 'partclone',
+'partclone.btrfs' => 'partclone',
 'partclone.info' => 'partclone',
 'partclone.restore' => 'partclone',
 'partprobe' => 'parted',
@@ -203,8 +204,9 @@ if($mainOperation eq 'backup'){
 
                             if ($partitions{$partition}{literalType} eq 'vfat' || $partitions{$partition}{literalType} eq 'btrfs') {
                                 #we use partclone
-                                my $partcloneVersion = "partclone.$partitions{$partition}{literalType}";
-                                `$bin{$partcloneVersion} -c -s $partition -o "$directory/partition_${partitionNumber}.img" >> $logfile 2>&1`;
+                                my $partcloneVersion = 'partclone.' . $partitions{$partition}{literalType};
+                                `echo "Using partclone binary: $partcloneVersion" >> $logfile 2>&1`;
+                                `$bin{"$partcloneVersion"} -c -s $partition -o "$directory/partition_${partitionNumber}.img" >> $logfile 2>&1`;
                                 $error = $? >> 8;
                                 `echo "Error code: $error" >> $logfile 2>&1`;
 
